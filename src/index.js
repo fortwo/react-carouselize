65643fcc8e26c1b12923b051ce7f1cf9fc2a497f
johan.v.gustafsson <johan.v.gustafsson@husqvarnagroup.com>
2019-10-03 15:48:00 +0200

    #2 Functional component and hooks

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useCallback, useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 
 // Utils
@@ -7,131 +7,123 @@ import classNames from 'classnames';
 // Style
 import './index.css';
 
-class Carouselize extends React.Component {
-  constructor(props) {
-    super(props);
+const Carouselize = props => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const timeout = useRef(0);
 
-    this.state = {
-      current: 0,
+  const { animation, children, duration, navigation, enableNavigation } = props;
+
+  const doSetTimeout = () => {
+    timeout.current = setTimeout(() => {
+      setCurrentSlide(curr => (curr === children.length - 1 ? 0 : curr + 1));
+    }, duration);
+  };
+
+  const goTo = slideIndex => {
+    setCurrentSlide(slideIndex);
+  };
+
+  const goToPrev = useCallback(() => {
+    setCurrentSlide(curr => (curr - 1 < 0 ? children.length - 1 : curr - 1));
+  }, [children]);
+
+  const goToNext = useCallback(() => {
+    setCurrentSlide(curr => (curr + 1) % children.length);
+  }, [children]);
+
+  const handleKeyDown = useCallback(
+    e => {
+      switch (e.keyCode) {
+        case 37:
+          goToPrev();
+          break;
+
+        case 39:
+          goToNext();
+          break;
+
+        default:
+          break;
+      }
+    },
+    [goToNext, goToPrev]
+  );
+
+  //Clear and Set timeout when currentSlide is updated
+  useEffect(() => {
+    clearTimeout(timeout.current);
+    doSetTimeout();
+  }, [currentSlide]);
+
+  //Bind keydown listener on mount, remove listener on unmount (or when handleKeyDown changes)
+  useEffect(() => {
+    if (props.enableKeys) {
+      document.addEventListener('keydown', handleKeyDown);
+
+      return () => {
+        document.removeEventListener('keydown', handleKeyDown);
+      };
+    }
+  }, [handleKeyDown]);
+
+  // Remove timeout on unmount
+  useEffect(() => {
+    return () => {
+      clearTimeout(timeout.current);
     };
+  }, []);
 
-    this.setInterval = this.setInterval.bind(this);
-    this.handleKeyDown = this.handleKeyDown.bind(this);
-    this.goTo = this.goTo.bind(this);
-    this.goToPrev = this.goToPrev.bind(this);
-    this.goToNext = this.goToNext.bind(this);
-  }
+  return (
+    <div className={classNames('carouselize', animation)}>
+      {children.map((child, index) => {
+        const previous =
+          (currentSlide === 0 && index === children.length - 1) ||
+          currentSlide === index + 1
+            ? 'previous'
+            : '';
+        const selected = currentSlide === index ? 'selected' : '';
+        const ready = !previous && !selected ? 'ready' : '';
+        return React.cloneElement(child, {
+          key: index,
+          className: classNames(
+            child.props.className,
+            'slide',
+            previous,
+            selected,
+            ready
+          ),
+        });
+      })}
 
-  componentDidMount() {
-    this.setInterval();
-
-    if (this.props.enableKeys) {
-      document.addEventListener('keydown', this.handleKeyDown);
-    }
-  }
-
-  componentWillUnmount() {
-    if (this.props.enableKeys) {
-      document.removeEventListener('keydown', this.handleKeyDown);
-    }
-  }
-
-  handleKeyDown(e) {
-    switch (e.keyCode) {
-      case 37:
-        this.goToPrev();
-        break;
-
-      case 39:
-        this.goToNext();
-        break;
-
-      default:
-        break;
-    }
-  }
-
-  setInterval() {
-    this.interval = setInterval(() => {
-      this.setState({
-        current: this.state.current === (this.props.children.length - 1) ? 0 : this.state.current + 1,
-      });
-    }, this.props.duration);
-  }
-
-  goTo(current) {
-    if (this.props.enableNavigation) {
-      clearInterval(this.interval);
-  
-      this.setState(
-        {
-          current,
-        },
-        () => {
-          this.setInterval();
-        },
-      );
-    }
-  }
-
-  goToPrev() {
-    clearInterval(this.interval);
-
-    this.setState(
-      {
-        current: this.state.current === 0 ? this.props.children.length - 1 : this.state.current - 1,
-      },
-      () => {
-        this.setInterval();
-      },
-    );
-  }
-
-  goToNext() {
-    clearInterval(this.interval);
-
-    this.setState(
-      {
-        current: this.state.current === this.props.children.length - 1 ? 0 : this.state.current + 1,
-      },
-      () => {
-        this.setInterval();
-      },
-    );
-  }
-
-  render() {
-    const { animation, children, navigation, enableNavigation } = this.props;
-    const { current } = this.state;
-
-    const items = React.Children.toArray(children);
-
-    return (
-      <div className={classNames('carouselize', animation)}>
-        {items.map((child, index) => {
-          const previous = (current === 0 && index === children.length - 1) || current === index + 1 ? 'previous' : '';
-          const selected = current === index ? 'selected' : '';
-          const ready = !previous && !selected ? 'ready' : '';
-          return React.cloneElement(child, {
-            key: index,
-            className: classNames(child.props.className, 'slide', previous, selected, ready),
-          });
-        })}
-
-        {navigation &&
-          <nav className={classNames('navigation', navigation, enableNavigation && 'enabled')}>
-            {items.map((child, index) => {
-              const selected = current === index ? 'selected' : '';
-              return (
-                <div key={index} className={classNames('bullet', selected)} onClick={() => this.goTo(index)}>&bull;</div>
-              );
-            })}
-          </nav>
-        }
-      </div>
-    );
-  }
-}
+      {navigation && (
+        <nav
+          className={classNames(
+            'navigation',
+            navigation,
+            enableNavigation && 'enabled'
+          )}
+        >
+          {children.map((child, index) => {
+            const selected = currentSlide === index ? 'selected' : '';
+            return (
+              <div
+                key={index}
+                className={classNames('bullet', selected)}
+                onClick={() => {
+                  if (enableNavigation) {
+                    goTo(index);
+                  }
+                }}
+              >
+                &bull;
+              </div>
+            );
+          })}
+        </nav>
+      )}
+    </div>
+  );
+};
 
 Carouselize.propTypes = {
   duration: PropTypes.number,
